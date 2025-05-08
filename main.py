20bc5be46f56036772aee2f25c4383fc9f7e0897
seojaehong <iceamericano9@gmail.com>
2025-05-08 23:02:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 import sys
 
-# 이 메시지는 표준 에러로 출력될 것입니다.
-print("Test script started successfully!", file=sys.stderr)
+# 이 메시지는 표준 출력으로 보내지므로 newsletter.txt 파일에 저장될 것입니다.
+print("Hello standard output")
 
-# 스크립트가 성공적으로 실행되었음을 나타내는 코드 0으로 종료
-sys.exit(0)
+# 이 메시지는 표준 에러로 보내지므로 error.log 파일에 저장될 것입니다.
+print("Hello standard error", file=sys.stderr)
+
+# 스크립트가 자연스럽게 종료되도록 sys.exit(0)는 사용하지 않습니다.
